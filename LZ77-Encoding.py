2bc4eaa47cac6f7d8fcc09c2b6d6c0451e78da23
Dev1ka <devika.kugan@gmail.com>
2026-05-15 21:33:01 +1000

    created longest_match function
    - replaced function placeholder comments w/ looping through window size to find and return longest match between window and look ahead buffer

--- LZ77-Encoding.py
+++ LZ77-Encoding.py
@@ -1,15 +1,48 @@
+"""
+=====================
+FINDING LONGEST MATCH
+=====================
+"""
+
+
 def longest_match(data, cursor, window_size, look_ahead_size):
+    """
+    Finds longest match within window and look ahead buffer (section to the right of given cursor)
+    & returns longest match and dist from cursor to assist in building the LZ77 triple in main loop
+    """
+
     best_dist = 0
     best_len = 0
 
-    '''
-    FUNCTION:
-    Loop through the search buffer to find the longest match with the look-ahead buffer,
-    return match length and distance from cursor to all instances of pattern
-    '''
+    # SEARCHING LENGTH OF WINDOW
+    for i in range(1, window_size + 1):
+        match_index = cursor - i
+
+        if match_index < 0:
+            break
+
+        current_len = 0
+        while current_len < look_ahead_size and (cursor + current_len) < len(data):
+            # FINDING MATCHES OF CHARS IN LOOKAHEAD & WINDOW
+            if data[match_index + current_len] == data[cursor + current_len]:
+                current_len += 1
+
+            else:
+                break
+
+        # SAVING LONGEST MATCH
+        if current_len > best_len:
+            best_len = current_len
+            best_dist = i
 
     return best_dist, best_len
 
+
+"""
+=====================
+ENCODING MATCHES
+=====================
+"""
 
 with open('LZ77-Compression.txt', 'w') as file:
     window_size = 0
