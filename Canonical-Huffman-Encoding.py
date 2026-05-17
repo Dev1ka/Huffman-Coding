0bb367677c0e710adf05607bf20c378775587b27
Dev1ka <devika.kugan@gmail.com>
2026-05-18 09:02:18 +1000

    organised into functions

--- Canonical-Huffman-Encoding.py
+++ Canonical-Huffman-Encoding.py
@@ -74,134 +74,134 @@ class Node:
         return self.freq < other.freq
 
 
-# CREATING FREQUENCY TABLE
-freq = {}
+def build_huffman_tree(text):
+    # CREATING FREQUENCY TABLE
+    freq = {}
 
-for char in text:
-    freq[char] = freq.get(char, 0) + 1
+    for char in text:
+        freq[char] = freq.get(char, 0) + 1
 
-for key, val in freq.items():
-    heapq.heappush(min_heap, (val, Node(key, val)))
-heapq.heappush(min_heap, (1, Node('EOF', 1)))
+    for key, val in freq.items():
+        heapq.heappush(min_heap, (val, Node(key, val)))
+    heapq.heappush(min_heap, (1, Node('EOF', 1)))
 
-# CONSTRUCTING BINARY TREE
-while len(min_heap) != 1:
-    node_1 = heapq.heappop(min_heap)[1]
-    node_2 = heapq.heappop(min_heap)[1]
+    # CONSTRUCTING BINARY TREE
+    while len(min_heap) != 1:
+        node_1 = heapq.heappop(min_heap)[1]
+        node_2 = heapq.heappop(min_heap)[1]
 
-    sum = node_1.freq + node_2.freq
-    root = Node(None, sum)
-    root.left = node_1
-    root.right = node_2
+        sum = node_1.freq + node_2.freq
+        root = Node(None, sum)
+        root.left = node_1
+        root.right = node_2
 
-    heapq.heappush(min_heap, (sum, root))
+        heapq.heappush(min_heap, (sum, root))
 
-# ASSIGNING BINARY CODES
-code = {}
-lengths = {}
-root = heapq.heappop(min_heap)[1]
+    # ASSIGNING BINARY CODES
+    code = {}
+    lengths = {}
+    root = heapq.heappop(min_heap)[1]
 
-assign_codes(root, '', code)
+    assign_codes(root, '', code)
 
-for key, val in code.items():
-    lengths[key] = len(val)
+    for key, val in code.items():
+        lengths[key] = len(val)
 
-"""
-=========================
-ASSIGNING CANONICAL CODES
-=========================
-"""
+    return lengths
 
-# FINDING CANONICAL HUFFMAN CODES
-sorted_dict = sorted(lengths.items(), key=lambda x: (x[1], x[0]))
-sorted_chars = [x[0] for x in sorted_dict]
-sorted_lens = [y[1] for y in sorted_dict]
 
-codes = []
-chars = []
-current_code = 0
-prev = 0
+def get_canonical_codes(lengths):
+    # FINDING CANONICAL HUFFMAN CODES
+    sorted_dict = sorted(lengths.items(), key=lambda x: (x[1], x[0]))
+    sorted_chars = [x[0] for x in sorted_dict]
+    sorted_lens = [y[1] for y in sorted_dict]
 
-for i in range(len(sorted_chars)):
-    char = sorted_chars[i]
-    length = sorted_lens[i]
+    codes = []
+    chars = []
+    current_code = 0
+    prev = 0
 
-    if prev > 0:
-        current_code <<= (length - prev)
+    for i in range(len(sorted_chars)):
+        char = sorted_chars[i]
+        length = sorted_lens[i]
 
-    chars.append(char)
-    codes.append(f"{current_code:0{length}b}")
+        if prev > 0:
+            current_code <<= (length - prev)
 
-    current_code += 1
-    prev = length
+        chars.append(char)
+        codes.append(f"{current_code:0{length}b}")
 
-"""
-========================
-CREATING COMPRESSED FILE
-========================
-"""
+        current_code += 1
+        prev = length
 
-with open(output_file, "wb") as file:
-    code_lookup = dict(zip(chars, codes))
+    return dict(zip(chars, codes))
 
-    # WRITING DECODING INFO
-    header = bytearray()
 
-    # byte 0 (total number of chars)
-    header.append(len(code_lookup))
+def write_compressed(output_file, text, code_lookup):
+    with open(output_file, "wb") as file:
+        # WRITING DECODING INFO
+        header = bytearray()
 
-    # ASCII val, bit-length
-    for char, code in code_lookup.items():
-        # find integer val of token
-        if char == 'EOF':
-            token_val = 256
-        else:
-            token_val = ord(char)  # convert character to its 0-255 integer value
+        # byte 0 (total number of chars)
+        header.append(len(code_lookup))
 
-        # split the 16-bit integer into two 8-bit bytes
-        high_byte = (token_val >> 8) & 0xFF  # 00000001 for EOF, 00000000 for everything else
-        low_byte = token_val & 0xFF
+        # ASCII val, bit-length
+        for char, code in code_lookup.items():
+            # find integer val of token
+            if char == 'EOF':
+                token_val = 256
+            else:
+                token_val = ord(char)  # convert character to its 0-255 integer value
 
-        # write into header arr
-        header.append(high_byte)
-        header.append(low_byte)
-        header.append(len(code))  # stores bit-length as a single byte
+            # split the 16-bit integer into two 8-bit bytes
+            high_byte = (token_val >> 8) & 0xFF  # 00000001 for EOF, 00000000 for everything else
+            low_byte = token_val & 0xFF
 
-    file.write(header)
+            # write into header arr
+            header.append(high_byte)
+            header.append(low_byte)
+            header.append(len(code))  # stores bit-length as a single byte
 
-    # ENCODING REST OF FILE
-    buffer = 0
-    count = 0
+        file.write(header)
 
-    for i in text:
-        current = code_lookup[i]
-        for x in current:
+        # ENCODING REST OF FILE
+        buffer = 0
+        count = 0
+
+        for i in text:
+            current = code_lookup[i]
+            for x in current:
+                buffer <<= 1
+                buffer |= int(x)
+                count += 1
+
+                if count == 8:
+                    file.write(bytes([buffer]))
+                    buffer = 0
+                    count = 0
+
+        # HANDLING OVERFLOW
+        end_code = code_lookup['EOF']
+        for y in end_code:
             buffer <<= 1
-            buffer |= int(x)
+            buffer |= int(y)
             count += 1
 
             if count == 8:
                 file.write(bytes([buffer]))
                 buffer = 0
                 count = 0
-
-    # HANDLING OVERFLOW
-    end_code = code_lookup['EOF']
-    for y in end_code:
-        buffer <<= 1
-        buffer |= int(y)
-        count += 1
-
-        if count == 8:
+        if count != 0:
+            while count < 8:
+                buffer <<= 1
+                buffer |= 0
+                count += 1
             file.write(bytes([buffer]))
-            buffer = 0
-            count = 0
-    if count != 0:
-        while count < 8:
-            buffer <<= 1
-            buffer |= 0
-            count += 1
-        file.write(bytes([buffer]))
+
+
+lengths = build_huffman_tree(text)
+code_lookup = get_canonical_codes(lengths)
+write_compressed(output_file, text, code_lookup)
 
 end_time = time.time()
 compressed_size = os.path.getsize(output_file)
